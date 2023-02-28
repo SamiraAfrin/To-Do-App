2e0ea84cc16e89c5a8e3d066c9d9bc8053dd260d
SamiraAfrin <samira.afrin.alam.shopnil@g.bracu.ac.bd>
2023-02-28 13:23:07 +0600

    implemented unit test for user usecase, taskByID and alltask unit test for delivery layer

--- Task/delivery/http/task_handler_test.go
+++ Task/delivery/http/task_handler_test.go
@@ -59,7 +59,92 @@ func TestCreateTask(t *testing.T) {
 
 }
 
-func TestDelete(t *testing.T) {
+func TestGetTaskByID(t *testing.T) {
+	var mockTask models.Task
+	err := faker.FakeData(&mockTask)
+	assert.NoError(t, err)
+
+	mockUCase := new(mocks.Usecase)
+
+	num := int(mockTask.ID)
+	mockUCase.On("GetByID", mock.Anything, int64(num)).Return(&mockTask, nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.GET, "/tasks/"+strconv.Itoa(num), strings.NewReader(""))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("tasks/:ID")
+	c.SetParamNames("ID")
+	c.SetParamValues(strconv.Itoa(num))
+	handler := TaskHandler{
+		TaskUsecase: mockUCase,
+	}
+	err = handler.GetByID(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	mockUCase.AssertExpectations(t)
+}
+
+func TestGetAllTask(t *testing.T) {
+	var mockTask models.Task
+	err := faker.FakeData(&mockTask)
+	assert.NoError(t, err)
+
+	mockUCase := new(mocks.Usecase)
+	mockListTask := make([]*models.Task, 0)
+	mockListTask = append(mockListTask, &mockTask)
+
+	mockUCase.On("GetAllTask", mock.Anything).Return(mockListTask, nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.GET, "/tasks/", strings.NewReader(""))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	handler := TaskHandler{
+		TaskUsecase: mockUCase,
+	}
+	err = handler.GetAllTask(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	mockUCase.AssertExpectations(t)
+
+}
+
+//func TestGetTasksByUserID(t *testing.T) {
+//	var mockTask models.Task
+//	err := faker.FakeData(&mockTask)
+//	assert.NoError(t, err)
+//
+//	mockUCase := new(mocks.Usecase)
+//	mockListTask := make([]*models.Task, 0)
+//	mockListTask = append(mockListTask, &mockTask)
+//	userID := 1
+//	mockUCase.On("GetByUserID", mock.Anything, int64(userID)).Return(mockListTask, nil)
+//
+//	e := echo.New()
+//	req, err := http.NewRequest(echo.GET, "/tasks/user/"+strconv.Itoa(userID), strings.NewReader(""))
+//	assert.NoError(t, err)
+//
+//	rec := httptest.NewRecorder()
+//	c := e.NewContext(req, rec)
+//	handler := TaskHandler{
+//		TaskUsecase: mockUCase,
+//	}
+//	err = handler.GetByUserID(c)
+//	require.NoError(t, err)
+//
+//	assert.Equal(t, http.StatusOK, rec.Code)
+//	mockUCase.AssertExpectations(t)
+//
+//}
+
+/*func TestDelete(t *testing.T) {
 	var mockTask models.Task
 	err := faker.FakeData(&mockTask)
 	assert.NoError(t, err)
@@ -89,4 +174,4 @@ func TestDelete(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, rec.Code)
 	mockUCase.AssertExpectations(t)
-}
+}*/
---
+++ User/usecase/user_usecase_test.go
@@ -0,0 +1,87 @@
+package usecase
+
+import (
+	//"To_Do_App/User"
+	"To_Do_App/User/mocks"
+	"To_Do_App/models"
+	"context"
+	"errors"
+
+	//"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateUser(t *testing.T) {
+	mockUserRepo := new(mocks.Repository)
+
+	mockUser := models.User{
+		Name: "First User",
+	}
+	t.Run("success", func(t *testing.T) {
+		tMockUser := mockUser
+		tMockUser.ID = 1
+
+		mockUserRepo.On("Store", mock.Anything, mock.AnythingOfType("*models.User")).
+			Return(nil).Once()
+
+		u := NewUserUsecase(mockUserRepo, time.Second*2)
+
+		err := u.Store(context.TODO(), &tMockUser)
+
+		assert.NoError(t, err)
+		mockUserRepo.AssertExpectations(t)
+	})
+}
+
+func TestGetAllUser(t *testing.T) {
+	mockUserRepo := new(mocks.Repository)
+	mockUser := &models.User{}
+	mockListUser := make([]*models.User, 0)
+	mockListUser = append(mockListUser, mockUser)
+
+	t.Run("success", func(t *testing.T) {
+		mockUserRepo.On("GetAllUser", mock.Anything).
+			Return(mockListUser, nil).Once()
+
+		u := NewUserUsecase(mockUserRepo, time.Second*2)
+		_, err := u.GetAllUser(context.TODO())
+
+		assert.NoError(t, err)
+		mockUserRepo.AssertExpectations(t)
+	})
+
+	t.Run("not-found", func(t *testing.T) {
+		mockUserRepo.On("GetAllUser", mock.Anything).
+			Return(mockListUser, errors.New("user table doesn't have any data")).Once()
+
+		u := NewUserUsecase(mockUserRepo, time.Second*2)
+		list, err := u.GetAllUser(context.TODO())
+
+		assert.Error(t, err)
+		assert.Len(t, list, 0)
+		mockUserRepo.AssertExpectations(t)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	mockUserRepo := new(mocks.Repository)
+	mockUser := &models.User{
+		Name: "Update name",
+	}
+
+	t.Run("success", func(t *testing.T) {
+
+		mockUserRepo.On("Update", mock.Anything, mockUser).
+			Return(nil).Once()
+
+		u := NewUserUsecase(mockUserRepo, time.Second*2)
+		err := u.Update(context.TODO(), mockUser)
+
+		assert.NoError(t, err)
+		mockUserRepo.AssertExpectations(t)
+	})
+}
