2ce56e41fd70517ff99b0e1d37e321477fcfde4d
SamiraAfrin <samira.afrin.alam.shopnil@g.bracu.ac.bd>
2023-02-28 13:43:20 +0600

    implemented unit test for the user delivery layer

--- Task/delivery/http/task_handler_test.go
+++ Task/delivery/http/task_handler_test.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"To_Do_App/Task/mocks"
+	userMock "To_Do_App/User/mocks"
 	"To_Do_App/models"
 	"encoding/json"
 	"net/http"
@@ -17,6 +18,101 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
+
+// for the user
+func TestCreateUser(t *testing.T) {
+
+	mockUser := models.User{
+		Name: "Hello",
+	}
+
+	tempMockUser := mockUser
+	tempMockUser.ID = 15
+
+	j, err := json.Marshal(tempMockUser)
+	assert.NoError(t, err)
+
+	mockUCase := new(userMock.Usecase)
+	mockUCase.On("Store", mock.Anything, mock.AnythingOfType("*models.User")).Return(nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.POST, "/users", strings.NewReader(string(j)))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/users")
+
+	handler := UserHandler{
+		UserUsecase: mockUCase,
+	}
+	err = handler.UserStore(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusCreated, rec.Code)
+	mockUCase.AssertExpectations(t)
+
+}
+
+func TestGetAllUser(t *testing.T) {
+	var mockUser models.User
+	err := faker.FakeData(&mockUser)
+	assert.NoError(t, err)
+
+	mockUCase := new(userMock.Usecase)
+	mockListUser := make([]*models.User, 0)
+	mockListUser = append(mockListUser, &mockUser)
+
+	mockUCase.On("GetAllUser", mock.Anything).Return(mockListUser, nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.GET, "users", strings.NewReader(""))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	handler := UserHandler{
+		UserUsecase: mockUCase,
+	}
+	err = handler.GetAllUser(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	mockUCase.AssertExpectations(t)
+
+}
+
+func TestUserUpdate(t *testing.T) {
+	var mockUser models.User
+	err := faker.FakeData(&mockUser)
+	assert.NoError(t, err)
+
+	mockUCase := new(userMock.Usecase)
+
+	num := int(mockUser.ID)
+	mockUCase.On("Update", mock.Anything, int64(num)).Return(nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.PUT, "/users/"+strconv.Itoa(num), strings.NewReader(""))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("users/:ID")
+	c.SetParamNames("ID")
+	c.SetParamValues(strconv.Itoa(num))
+	handler := UserHandler{
+		UserUsecase: mockUCase,
+	}
+	err = handler.UserUpdate(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	mockUCase.AssertExpectations(t)
+}
+
+// for the task
 
 func TestCreateTask(t *testing.T) {
 
@@ -100,7 +196,7 @@ func TestGetAllTask(t *testing.T) {
 	mockUCase.On("GetAllTask", mock.Anything).Return(mockListTask, nil)
 
 	e := echo.New()
-	req, err := http.NewRequest(echo.GET, "/tasks/", strings.NewReader(""))
+	req, err := http.NewRequest(echo.GET, "tasks", strings.NewReader(""))
 	assert.NoError(t, err)
 
 	rec := httptest.NewRecorder()
